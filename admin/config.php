292093d07527988985635a0710e6c5baec9fdb7e
akcode7 <ashishgotapogo10@gmail.com>
2023-05-30 13:30:31 +0530

    dark background with neon numbers

--- admin/config.php
+++ admin/config.php
@@ -6,7 +6,7 @@
 
  $conn = mysqli_connect($server, $username, $password, $database);
  if ($conn){
-    // echo "Success";
+   //  echo "Success";
  }
  else{
     die("Error" . mysqli_connect_error());
--- index.php
+++ index.php
@@ -14,20 +14,79 @@
       *{
     font-family: 'Poppins', sans serif;
       }
+      @font-face {
+            font-family: digital-font;
+            src: url(font/font-theme.ttf);
+        }
+
       .neo{
         border-radius: 24px;
         /* background: #e0e0e0; */
         box-shadow:  7px 7px 14px #d0d0d0,
         -7px -7px 14px #f0f0f0;
             }
+
+          /* .digital{
+              font-family:'digital-font';
+            } */
+
+            .neon-sell {
+                animation: neonAnim 2.5s alternate-reverse infinite;
+                color: #D22B2B;
+                font-family:'digital-font';
+            }
+            @keyframes neonAnim {
+                from {
+                    text-shadow:
+                      0 0 34px rgba(210, 43, 43, .7),
+                      0 0 64px rgba(210, 43, 43, .7),
+                      0 0 124px rgba(210, 43, 43, .7);
+                  }
+                  to {
+                    text-shadow:
+                      0 0 29px rgba(210, 43, 43, .7),
+                      0 0 44px rgba(210, 43, 43, .7),
+                      0 0 84px rgba(210, 43, 43, .7);
+                  }
+            }
+
+            .neon-buy {
+                animation: neonAnimation 2s alternate-reverse infinite;
+                color: #32CD32;
+                font-family:'digital-font';
+            }
+            @keyframes neonAnimation {
+            from {
+                text-shadow:
+                  0 0 34px rgba(50, 205, 50, .7),
+                  0 0 64px rgba(50, 205, 50, .7),
+                  0 0 124px rgba(50, 205, 50, .7);
+              }
+              to {
+                text-shadow:
+                  0 0 29px rgba(50, 205, 50, .7),
+                  0 0 44px rgba(50, 205, 50, .7),
+                  0 0 84px rgba(50, 205, 50, .7);
+              }
+        }
+
+        .shadow-box{
+          border-radius: 10px;
+          background: #0F172A;
+          box-shadow:  8px 8px 16px #070b15,
+             -8px -8px 16px #17233f;
+        }
     </style>
 </head>
-<body class="h-screen">
+<body class="h-screen bg-[#0F172A]">
 
-<div class="w-full py-8 ">
-  <div class="grid grid-cols-3">
-      <div class="col-span-3 md:col-span-1 px-10 flex justify-center md:justify-normal"><img src="img/sunny.jpg" alt="logo" class="w-56"></div>
-      <div class="flex col-span-3 md:col-span-2 px-14 justify-center md:justify-normal md:pt-24"> <p class="text-2xl md:text-4xl font-bold pt-3 pl-3 flex justify-center items-center md:-mt-16 md:pb-7">Sunny Bureau de Change</p></div>
+<div class="w-full py-2">
+  <div class="grid grid-cols-4">
+      <div class="col-span-4 md:col-span-1 px-10 flex justify-center md:justify-normal"><img src="img/logo-transparent.png" alt="logo" class="w-52"></div>
+      <div class="flex col-span-4 md:col-span-3 px-1 justify-center md:justify-normal md:pt-24"> <p class="text-2xl text-white md:text-4xl xl:text-5xl  font-bold pt-3 flex justify-center items-center md:-mt-16 md:pb-1 2xl:pl-36">Sunny Bureau de Change</p></div>
+  </div>
+  <div class="container mx-auto">
+      <p class="text-center text-white text-2xl md:text-5xl xl:text-5xl font-bold -mt-7">TODAYS FOREIGN EXCHANGE RATES:</p>
   </div>
 </div>
 
@@ -40,166 +99,166 @@
       </div>
   </div> -->
   <!-- currency starts -->
-  <div class="container mx-auto p-8 md:p-8 mt-5">
+<div class="container mx-auto p-16 md:p-8 mt-5">
     
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 ">
-        <thead class="text-xs text-gray-700 uppercase">
-            <tr class="border-b border-gray-200">
-                <th scope="col" class="px-6 py-3 font-semibold text-lg bg-gray-50 w-2/5">
-                Currency
-                </th>
-                <th scope="col" class="px-6 py-3 font-semibold text-lg text-white bg-green-500 text-center">
-                    Buy
-                </th>
-                <th scope="col" class="px-6 py-3 font-semibold text-lg text-white bg-red-500 text-center">
-                    Sell
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="border-b border-gray-200">
-                <th scope="row" class="px-6 py-4 md:flex font-medium text-gray-900 whitespace-nowrap bg-gray-50">
-                    <img src="img/british.png" class="w-32" alt="" srcset="">
-                    <span class="font-bold text-xl md:p-6 mx-auto flex justify-center md:mx-0">GBP</span>
-                </th>
-                <td class="px-6 py-4 text-center font-semibold text-lg">
-                <!-- buy price -->
-              <?php $sql = 'SELECT `buyprice` FROM `currency`  WHERE `sno` = 1';
-                $result = mysqli_query($conn, $sql);
-                if($result){
-                  $row = mysqli_fetch_assoc($result);
-                    echo $row['buyprice'];
-                }
-                else{
-                  echo 'error';
-                } ?>
-                <!-- buy price -->
-                </td>
-                
-                <td class="px-6 py-4 text-center bg-gray-50 font-semibold text-lg">
-                    <!-- sell price -->
-            <?php $sql = 'SELECT `sellprice` FROM `currency`  WHERE `sno` = 1';
-                $result = mysqli_query($conn, $sql);
-                if($result){
-                  $row = mysqli_fetch_assoc($result);
-                    echo $row['sellprice'];
-                }
-                else{
-                  echo 'error';
-                } ?>
-                <!-- sell price -->
-                </td>
-            </tr>
-            <tr class="border-b border-gray-200">
-                <th scope="row" class="px-6 py-4 col md:flex font-medium text-gray-900 whitespace-nowrap bg-gray-50">
-                <img src="img/usa.png" class="w-32" alt="" srcset="">
-                    <span class="font-bold text-xl md:p-6 mx-auto flex justify-center md:mx-0">USD</span>
-                </th>
-                <td class="px-6 py-4 text-center font-semibold text-lg">
-                <!-- buy price -->
-              <?php $sql = 'SELECT `buyprice` FROM `currency`  WHERE `sno` = 2';
-                $result = mysqli_query($conn, $sql);
-                if($result){
-                  $row = mysqli_fetch_assoc($result);
-                    echo $row['buyprice'];
-                }
-                else{
-                  echo 'error';
-                } ?>
-                <!-- buy price -->
-                </td>
-             
-                <td class="px-6 py-4 text-center bg-gray-50 font-semibold text-lg">
-                    <!-- sell price -->
-            <?php $sql = 'SELECT `sellprice` FROM `currency`  WHERE `sno` = 2';
-                $result = mysqli_query($conn, $sql);
-                if($result){
-                  $row = mysqli_fetch_assoc($result);
-                    echo $row['sellprice'];
-                }
-                else{
-                  echo 'error';
-                } ?>
-                <!-- sell price -->
-                </td>
-            </tr>
-            <tr class="border-b border-gray-200">
-                <th scope="row" class="px-6 py-4 col md:flex font-medium text-gray-900 whitespace-nowrap bg-gray-50">
-                <img src="img/europe.png" class="w-32" alt="" srcset="">
-                    <span class="font-bold text-xl md:p-6 mx-auto flex justify-center md:mx-0">EUR</span>
-                </th>
-                <td class="px-6 py-4 text-center font-semibold text-lg">
-                <!-- buy price -->
-              <?php $sql = 'SELECT `buyprice` FROM `currency`  WHERE `sno` = 3';
-                $result = mysqli_query($conn, $sql);
-                if($result){
-                  $row = mysqli_fetch_assoc($result);
-                    echo $row['buyprice'];
-                }
-                else{
-                  echo 'error';
-                } ?>
-                <!-- buy price -->
-                </td>
+  <div class="relative overflow-x-auto shadow-box">
+      <table class="w-full text-sm text-left">
+          <thead class="text-xs uppercase">
+              <tr class="border-b border-gray-200">
+                  <th scope="col" class="px-6 py-3 font-extrabold text-white text-4xl bg-[#0F172A] w-2/5">
+                  Currency
+                  </th>
+                  <th scope="col" class="px-6 py-3 font-extrabold text-4xl text-white bg-[#0F172A] text-center">
+                      Buy
+                  </th>
+                  <th scope="col" class="px-6 py-3 font-extrabold text-4xl text-white bg-[#0F172A] text-center">
+                      Sell
+                  </th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr class="border-b border-gray-200">
+                  <th scope="row" class="px-6 py-4 md:flex font-medium text-gray-900 whitespace-nowrap bg-[#0F172A]">
+                      <img src="img/british.png" class="w-32" alt="" srcset="">
+                      <span class="font-bold text-3xl text-white md:p-6 mx-auto flex justify-center md:mx-0">GBP</span>
+                  </th>
+                  <td class="px-6 py-4 text-center font-light text-5xl neon-buy border border-white">
+                  <!-- buy price -->
+                <?php $sql = 'SELECT `buyprice` FROM `currency`  WHERE `sno` = 1';
+                  $result = mysqli_query($conn, $sql);
+                  if($result){
+                    $row = mysqli_fetch_assoc($result);
+                      echo $row['buyprice'];
+                  }
+                  else{
+                    echo 'error';
+                  } ?>
+                  <!-- buy price -->
+                  </td>
+                  
+                  <td class="px-6 py-4 text-center font-light text-5xl neon-sell">
+                      <!-- sell price -->
+              <?php $sql = 'SELECT `sellprice` FROM `currency`  WHERE `sno` = 1';
+                  $result = mysqli_query($conn, $sql);
+                  if($result){
+                    $row = mysqli_fetch_assoc($result);
+                      echo $row['sellprice'];
+                  }
+                  else{
+                    echo 'error';
+                  } ?>
+                  <!-- sell price -->
+                  </td>
+              </tr>
+              <tr class="border-b border-gray-200">
+                  <th scope="row" class="px-6 py-4 col md:flex font-medium text-gray-900 whitespace-nowrap bg-[#0F172A]">
+                  <img src="img/usa.png" class="w-32" alt="" srcset="">
+                      <span class="font-bold text-3xl text-white md:p-6 mx-auto flex justify-center md:mx-0">USD</span>
+                  </th>
+                  <td class="px-6 py-4 text-center font-light text-5xl neon-buy border border-white">
+                  <!-- buy price -->
+                <?php $sql = 'SELECT `buyprice` FROM `currency`  WHERE `sno` = 2';
+                  $result = mysqli_query($conn, $sql);
+                  if($result){
+                    $row = mysqli_fetch_assoc($result);
+                      echo $row['buyprice'];
+                  }
+                  else{
+                    echo 'error';
+                  } ?>
+                  <!-- buy price -->
+                  </td>
               
-                <td class="px-6 py-4 text-center bg-gray-50 font-semibold text-lg">
-                    <!-- sell price -->
-            <?php $sql = 'SELECT `sellprice` FROM `currency`  WHERE `sno` = 3';
-                $result = mysqli_query($conn, $sql);
-                if($result){
-                  $row = mysqli_fetch_assoc($result);
-                    echo $row['sellprice'];
-                }
-                else{
-                  echo 'error';
-                } ?>
-                <!-- sell price -->
-                </td>
-            </tr>
-            <tr class="border-b border-gray-200">
-                <th scope="row" class="px-6 py-4 col md:flex font-medium text-gray-900 whitespace-nowrap bg-gray-50">
-                <img src="img/south-africa.png" class="w-32" alt="" srcset="">
-                    <span class="font-bold text-xl md:p-6 mx-auto flex justify-center md:mx-0">ZAR</span>
-                </th>
-                <td class="px-6 py-4 text-center font-semibold text-lg">
-                <!-- buy price -->
-              <?php $sql = 'SELECT `buyprice` FROM `currency`  WHERE `sno` = 4';
-                $result = mysqli_query($conn, $sql);
-                if($result){
-                  $row = mysqli_fetch_assoc($result);
-                    echo $row['buyprice'];
-                }
-                else{
-                  echo 'error';
-                } ?>
-                <!-- buy price -->
-                </td>
-               
-                <td class="px-6 py-4 text-center bg-gray-50 font-semibold text-lg">
-                    <!-- sell price -->
-            <?php $sql = 'SELECT `sellprice` FROM `currency`  WHERE `sno` = 4';
-                $result = mysqli_query($conn, $sql);
-                if($result){
-                  $row = mysqli_fetch_assoc($result);
-                    echo $row['sellprice'];
-                }
-                else{
-                  echo 'error';
-                } ?>
-                <!-- sell price -->
-                </td>
-            </tr>
-          
-        </tbody>
-    </table>
-    <span class="text-2xl font-bold justify-end flex pr-5 py-6"><?php $date = date("dS F Y"); echo "$date" ?></span> 
-</div>
-
+                  <td class="px-6 py-4 text-center font-light text-5xl neon-sell">
+                      <!-- sell price -->
+              <?php $sql = 'SELECT `sellprice` FROM `currency`  WHERE `sno` = 2';
+                  $result = mysqli_query($conn, $sql);
+                  if($result){
+                    $row = mysqli_fetch_assoc($result);
+                      echo $row['sellprice'];
+                  }
+                  else{
+                    echo 'error';
+                  } ?>
+                  <!-- sell price -->
+                  </td>
+              </tr>
+              <tr class="border-b border-gray-200">
+                  <th scope="row" class="px-6 py-4 col md:flex font-medium text-gray-900 whitespace-nowrap bg-[#0F172A]">
+                  <img src="img/europe.png" class="w-32" alt="" srcset="">
+                      <span class="font-bold text-3xl text-white md:p-6 mx-auto flex justify-center md:mx-0">EUR</span>
+                  </th>
+                  <td class="px-6 py-4 text-center font-light text-5xl neon-buy border border-white">
+                  <!-- buy price -->
+                <?php $sql = 'SELECT `buyprice` FROM `currency`  WHERE `sno` = 3';
+                  $result = mysqli_query($conn, $sql);
+                  if($result){
+                    $row = mysqli_fetch_assoc($result);
+                      echo $row['buyprice'];
+                  }
+                  else{
+                    echo 'error';
+                  } ?>
+                  <!-- buy price -->
+                  </td>
+                
+                  <td class="px-6 py-4 text-center font-light text-5xl neon-sell">
+                      <!-- sell price -->
+              <?php $sql = 'SELECT `sellprice` FROM `currency`  WHERE `sno` = 3';
+                  $result = mysqli_query($conn, $sql);
+                  if($result){
+                    $row = mysqli_fetch_assoc($result);
+                      echo $row['sellprice'];
+                  }
+                  else{
+                    echo 'error';
+                  } ?>
+                  <!-- sell price -->
+                  </td>
+              </tr>
+              <tr class="border-b border-gray-200">
+                  <th scope="row" class="px-6 py-4 col md:flex font-medium text-gray-900 whitespace-nowrap bg-[#0F172A]">
+                  <img src="img/south-africa.png" class="w-32" alt="" srcset="">
+                      <span class="font-bold text-3xl text-white md:p-6 mx-auto flex justify-center md:mx-0">ZAR</span>
+                  </th>
+                  <td class="px-6 py-4 text-center font-light text-5xl neon-buy border border-white">
+                  <!-- buy price -->
+                <?php $sql = 'SELECT `buyprice` FROM `currency`  WHERE `sno` = 4';
+                  $result = mysqli_query($conn, $sql);
+                  if($result){
+                    $row = mysqli_fetch_assoc($result);
+                      echo $row['buyprice'];
+                  }
+                  else{
+                    echo 'error';
+                  } ?>
+                  <!-- buy price -->
+                  </td>
+                
+                  <td class="px-6 py-4 text-center font-light text-5xl neon-sell">
+                      <!-- sell price -->
+              <?php $sql = 'SELECT `sellprice` FROM `currency`  WHERE `sno` = 4';
+                  $result = mysqli_query($conn, $sql);
+                  if($result){
+                    $row = mysqli_fetch_assoc($result);
+                      echo $row['sellprice'];
+                  }
+                  else{
+                    echo 'error';
+                  } ?>
+                  <!-- sell price -->
+                  </td>
+              </tr>
+            
+          </tbody>
+      </table>
+      <span class="text-3xl text-white font-bold justify-end flex pr-5 py-6"><?php $date = date("dS F Y"); echo "$date" ?></span> 
   </div>
+
+</div>
   <!-- currency ends -->
-  <div class="w-full bg-gray-100 p-5 absolute bottom-0">
-    <p class="text-gray-900 text-xl text-center font-semibold">Sunny Group Of Companies(c) Copyright-2023</p> 
+  <div class="w-full bg-[#0F172A] p-5   bottom-0 shadow-2xl shadow-gray-600">
+    <p class="text-white text-xl text-center font-semibold">Sunny Group Of Companies(c) Copyright-2023</p> 
   </div>
 
 </body>
